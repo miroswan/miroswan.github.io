4b4076dadf2700b5913b71b3f33a33887711dce2
Demitri Swan <dswan@digitalocean.com>
2019-01-29 15:36:33 +0000

    added better Technology view

--- src/views/RTechnology/RTechnology.tsx
+++ src/views/RTechnology/RTechnology.tsx
@@ -1,55 +1,89 @@
 import * as React from 'react';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
 
 import RBackground from '../../components/RBackground';
-import RTechList, { IRTechList } from '../../components/RTechList';
 
-const techLists: IRTechList[] = [
+const assetsPath: string = '../../../assets/';
+
+interface ICardData {
+  category: string;
+  items: string[];
+  image: string;
+}
+
+const techList: ICardData[] = [
   {
-    domain: 'Languages',
+    category: 'Languages',
+    image: assetsPath + 'golang.png',
     items: ['Python', 'Go', 'Ruby', 'TypeScript', 'Bash', 'Scala'],
   },
   {
-    domain: 'Containers',
+    category: 'Containers',
+    image: assetsPath + 'k8.png',
     items: ['Mesos', 'Kubernetes', 'Docker'],
   },
   {
-    domain: 'Configuration Management',
+    category: 'Configuration Management',
+    image: assetsPath + 'ansible.png',
     items: ['Chef', 'Ansible', 'Python'],
   },
   {
-    domain: 'CI/CD',
+    category: 'CI/CD',
+    image: assetsPath + 'concourse.png',
     items: ['ConcourseCI', 'Drone', 'TravisCI', 'AppVeyor', 'Jenkins'],
   },
   {
-    domain: 'Operating Systems',
+    category: 'Operating Systems',
+    image: assetsPath + 'ubuntu.png',
     items: ['Linux', 'MacOS'],
   },
   {
-    domain: 'Frontend',
+    category: 'Frontend',
+    image: assetsPath + 'react.png',
     items: ['React', 'Material-UI', 'Webpack'],
   },
   {
-    domain: 'Architecture Components',
+    category: 'Architecture Components',
+    image: assetsPath + 'kafka.png',
     items: ['Kafka', 'Consul', 'Zookeeper'],
   },
   {
-    domain: 'Engineering References',
+    category: 'Knowledge Base',
+    image: assetsPath + 'algo.png',
     items: [
       'Algorithms - Robert Sedgewick',
       'Design Patterns - GoF',
       'Modern Operating Systems - Andrew S. Tanenbaum',
       'Site Reliability Engineering - Google',
       'Systems Performance - Brendan Gregg',
+      'Unix and Linux System Administration Handbook',
     ],
   },
 ];
 
 const styles = createStyles({
-  grid: {
+  card: {
+    width: '100%',
+  },
+  cardContainer: {
+    marginBottom: '2%',
     marginRight: '40%',
+    marginTop: '2%',
+  },
+  media: {
+    display: 'block',
+    height: 200,
+    marginBottom: '20px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 200,
   },
   root: {
     flexGrow: 1,
@@ -59,10 +93,18 @@ const styles = createStyles({
 export interface IRTechnologyProps extends WithStyles<typeof styles> {}
 
 const RTechnology: React.SFC<IRTechnologyProps> = (props: IRTechnologyProps): JSX.Element => {
-  const gridItems: JSX.Element[] = techLists.map((item: IRTechList, i: number) => {
+  const cards: JSX.Element[] = techList.map((item: ICardData, i: number) => {
     return (
-      <Grid item key={i}>
-        <RTechList domain={item.domain} items={item.items} />
+      <Grid key={i} item xs={4}>
+        <div className={props.classes.card}>
+          <img className={props.classes.media} src={item.image} />
+          <Typography gutterBottom variant='h5' component='h2' align='center'>
+            {item.category}
+          </Typography>
+          <Typography variant='body1' align='center'>
+            {item.items.map((entry: string, x: number) => <li key={x}>{entry}</li>)}
+          </Typography>
+        </div>
       </Grid>
     );
   });
@@ -70,9 +112,9 @@ const RTechnology: React.SFC<IRTechnologyProps> = (props: IRTechnologyProps): JS
   return (
     <div className={props.classes.root}>
       <RBackground />
-      <div className={props.classes.grid}>
-        <Grid container spacing={8}>
-          {gridItems}
+      <div className={props.classes.cardContainer}>
+        <Grid justify='center' container spacing={8}>
+        {cards}
         </Grid>
       </div>
     </div>
